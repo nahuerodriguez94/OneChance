3757fe95f18fd24613228db5f1e6514299acb5c3
Nahuel Rodriguez <mna.rodriguez94@gmail.com>
2025-01-22 12:13:39 -0300

    se creo getproducts y getproductId

--- back/src/controllers/product.controller.js
+++ back/src/controllers/product.controller.js
@@ -1,9 +1,34 @@
 const { Product } = require("../models/product.model.js");
 
+const getProducts = async (req, res) => {
+  try {
+    const response = await Product.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ status: "failure", message: error.message });
+  }
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response = await Product.findByPk(id);
+    if (response) {
+      return res.status(200).json(response);
+    } else {
+      res
+        .status(400)
+        .json({ status: "failure", message: "El producto no existe" });
+    }
+  } catch (error) {
+    res.status(500).json({ status: "failure", message: error, message });
+  }
+};
 
 const createProduct = async (req, res) => {
   const body = req.body;
- // console.log("Datos recibidos:" , body); // Comprobamos los datos por consola
+  // console.log("Datos recibidos:" , body); // Comprobamos los datos por consola
 
   try {
     const response = await Product.create(body);
@@ -11,13 +36,14 @@ const createProduct = async (req, res) => {
       .status(200)
       .json({ status: "Succes", message: `Usuario ID: ${response.id} creado` });
   } catch (error) {
-   // console.log("Error al crear el producto:", error); // registra el detalle del error 
+    // console.log("Error al crear el producto:", error); // registra el detalle del error
     res.status(500).json({ status: "failure", message: error.message });
   }
 };
 
-
 // Exportaciones
 module.exports = {
-    createProduct,
+  createProduct,
+  getProducts,
+  getProductById,
 };
--- back/src/routes/product.routes.js
+++ back/src/routes/product.routes.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const {
-    createProduct
+    createProduct,
+    getProducts,
+    getProductById,
 } = require ("../controllers/product.controller.js");
 const { validateProductData } = require("../middlewares/product.middleware.js");
 
 const router = Router();
 
 router.post("/",validateProductData, createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
 
 module.exports = router;
